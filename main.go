a0348cf65627ba4340e8045ce8a2ba6c78a8c421
james <jamessimeao@outlook.com>
2026-03-11 20:39:39 -0300

    Set an endpoint /ws for websocket connection

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello")
+	setupAPI()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setupAPI() {
+	manager := NewManager()
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/ws", manager.serveWS)
 }
